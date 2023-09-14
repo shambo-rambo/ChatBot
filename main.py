d493aa3d7ff57df32bc59a4a15b14aaf42cce29b
shambo-rambo <121490568+shambo-rambo@users.noreply.github.com>
2023-09-14 12:08:24 +1000

    Update main.py

--- main.py
+++ main.py
@@ -34,29 +34,21 @@ def main():
     st.header("History Essay Tutor 🤖")
 
     # sidebar with user input
-    with st.sidebar:
-        user_input = st.text_input("Your message: ", key="user_input")
-
-        # handle user input
+with st.sidebar:
+    # Set a default value for user_input in session state if it doesn't exist
     if 'user_input' not in st.session_state:
         st.session_state.user_input = ''
-    
+
+    # Create the widget
     user_input = st.text_input("Your message: ", value=st.session_state.user_input, key="user_input")
-    
+
+    # Handle user input
     if user_input:
         st.session_state.messages.append(HumanMessage(content=user_input))
         with st.spinner("Thinking..."):
             response = chat(st.session_state.messages)
         st.session_state.messages.append(AIMessage(content=response.content))
         st.session_state.user_input = ""  # Reset the user input in the session state
-    
-        # display message history, skipping the SystemMessage
-        messages = st.session_state.get('messages', [])
-        for i, msg in enumerate(messages[1:]):  # start from index 1 to skip the SystemMessage
-            if isinstance(msg, HumanMessage):
-                message(msg.content, is_user=True, key=str(i) + '_user')
-            elif isinstance(msg, AIMessage):
-                message(msg.content, is_user=False, key=str(i) + '_ai')
     
 if __name__ == '__main__':
     main()
