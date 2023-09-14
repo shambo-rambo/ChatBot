2d588b9d1f52ea0ca068181c7ef0306c8b8946c5
shambo-rambo <121490568+shambo-rambo@users.noreply.github.com>
2023-09-14 12:06:26 +1000

    Update main.py

--- main.py
+++ main.py
@@ -58,5 +58,5 @@ def main():
             elif isinstance(msg, AIMessage):
                 message(msg.content, is_user=False, key=str(i) + '_ai')
     
-    if __name__ == '__main__':
-        main()
+if __name__ == '__main__':
+    main()
